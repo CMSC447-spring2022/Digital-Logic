4837db19e2263e73b532c39fcccaf670444a419e
Kes Johnson <kes.j.johnson@gmail.com>
2022-04-10 19:43:05 -0400

    typo

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -2,6 +2,6 @@ from django.test import TestCase
 
 
 class SignUpViewTest(TestCase):
-    def test_login_page(self):
+    def test_signup_page(self):
         response = self.client.get('/accounts/signup/')
         self.assertEqual(response.status_code, 200)
