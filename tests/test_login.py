1f19ccd8981abb0614ee0bb2ec35cdac5106085b
Kes Johnson <kes.j.johnson@gmail.com>
2022-04-10 18:04:35 -0400

    login tests added

--- tests/test_login.py
+++ tests/test_login.py
@@ -25,21 +25,7 @@ class SigninTest(TestCase):
         self.assertFalse(user is not None and user.is_authenticated)
 
 
-class SignInViewTest(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(username='test', password='123abc456', email='test@example.com')
-
-    def tearDown(self):
-        self.user.delete()
-
-    def test_correct(self):
-        response = self.client.post('/login/', {'username': 'test', 'password': '123abc456'})
-        self.assertTrue(response.data['authenticated'])
-
-    def test_wrong_username(self):
-        response = self.client.post('/login/', {'username': 'wrong', 'password': '123abc456'})
-        self.assertFalse(response.data['authenticated'])
-
-    def test_wrong_pssword(self):
-        response = self.client.post('/login/', {'username': 'test', 'password': 'wrong'})
-        self.assertFalse(response.data['authenticated'])
+class LoginViewTest(TestCase):
+    def test_login_page(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
