0cb005e179f469531ac7412f8ad4460f678020fb
Kes Johnson <kes.j.johnson@gmail.com>
2022-04-11 11:42:20 -0400

    tests added for signup

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model, authenticate
 from django.test import TestCase
 
 
@@ -5,3 +6,26 @@ class SignUpViewTest(TestCase):
     def test_signup_page(self):
         response = self.client.get('/accounts/signup/')
         self.assertEqual(response.status_code, 200)
+
+
+class SignUpTest(TestCase):
+    def setUp(self):
+        self.username = 'testuser'
+        self.email = 'testuser@email.com'
+        self.password = 'digitallogic'
+
+    def tearDown(self):
+        user = authenticate(username='testuser', password='digitallogic')
+        user.delete()
+
+    def test_signup_form(self):
+        response = self.client.post('/accounts/signup/', data={
+            'username': self.username,
+            'email': self.email,
+            'password1': self.password,
+            'password2': self.password
+        })
+        self.assertEqual(response.status_code, 302)
+
+        users = get_user_model().objects.all()
+        self.assertEqual(users.count(), 1)
