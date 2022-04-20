5eb3ab1cb7dbfe798611f2e60728f76bbc80bdd8
Matt <mmchugh1@umbc.edu>
2022-04-20 19:16:05 -0400

    misc updates to html files

--- LoginPage/models.py
+++ LoginPage/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 
-class LoginContext(models.Model):
-    login_url = models.TextField()
-    instance_id = models.TextField(blank=True)
+# class LoginContext(models.l):
+#     login_url = models.TextField()
+#     instance_id = models.TextField(blank=True)
 
 
 # class ErrorContext(models.Model):
