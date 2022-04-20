321d85bb94ffc1c9d9949ca521387257fdbdd854
Kes Johnson <kes.j.johnson@gmail.com>
2022-04-20 11:05:00 -0400

    fixed secret path value

--- accounts/email_connection.py
+++ accounts/email_connection.py
@@ -1,5 +1,7 @@
+# https://realpython.com/python-send-email/
+
 import smtplib, ssl
-import json
+import json, os
 from os.path import exists
 import logging
 
@@ -7,37 +9,32 @@ logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:
 
 
 def initconn(debug):
+    secret_path = os.path.dirname(__file__) + '/../../secret'
     if debug:
-        logging.info("logging in to email with localhost")
-        port = 1025
-        stmp_server = 'localhost'
-        username = 'test@test.com'
-    elif exists('../../secret'):
+        logging.info("debug mode, not logging in")
+        return '', 'test@test.com'
+    elif exists(secret_path):
         logging.info("logging in to email with gmail")
-        with open('../../secret', 'rb') as configfile:
+        with open(secret_path, 'rb') as configfile:
             config = json.load(configfile)
         port = 465  # For SSL
         username = config['email']['username']
         password = config['email']['password']
         stmp_server = 'smtp.gmail.com'
     else:
-        logging.warning("no secret file found, defaulting to localhost for email login")
-        port = 1025
-        stmp_server = 'localhost'
-        username = 'test@test.com'
+        logging.warning("no secret file found, defaulting to debug mode for email login")
+        return '', 'test@test.com'
 
     # Create a secure SSL context
     context = ssl.create_default_context()
-    print('about to connect')
     server = smtplib.SMTP_SSL(stmp_server, port, context=context)
-    if stmp_server != 'localhost':
-        server.login(username, password)
-    print('connected')
+    server.login(username, password)
     return server, username
 
 
 class Email:
-    def __init__(self, debug=True):
+    def __init__(self, debug=False):
+        self.debug = debug
         self.conn, self.username = initconn(debug)
 
     def send_mail(self, to_email, message_subject, message_body):
@@ -49,5 +46,6 @@ class Email:
 
 
 if __name__ == '__main__':
+
     e = Email()
     print(e)
