467da9d21a1db6127773f9cb87b5b18dac0e870a
Kes Johnson <kes.j.johnson@gmail.com>
2022-04-21 16:29:24 -0400

    removed commented lines

--- DigitalLogic/settings.py
+++ DigitalLogic/settings.py
@@ -123,8 +123,6 @@ STATIC_URL = '/static/'
 if 'HEROKU' in os.environ:
     import django_heroku
     django_heroku.settings(locals())
-#import django_heroku
-#django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
