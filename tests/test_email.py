0f84012c32873a0f59d94ab7090ed6255c278214
Kes Johnson <kes.j.johnson@gmail.com>
2022-04-23 20:46:13 -0400

    fixed test for email in github

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,3 +1,5 @@
+import os
+
 from accounts.email_connection import Email
 from django.test import TestCase
 
@@ -6,8 +8,14 @@ class EmailTest(TestCase):
     def test_email_connection(self):
         email = Email()
         self.assertIsNotNone(email.conn)
-        self.assertEqual(email.debug, False)
-        self.assertIsNot(email.username, 'test@test.com')
+        if 'HEROKU' in os.environ:
+            self.assertEqual(email.debug, False)
+            self.assertIsNot(email.username, 'test@test.com')
+        else:
+            self.assertEqual(email.debug, True)
+            self.assertIs(email.username, 'test@test.com')
+
+
 
     def test_send_email(self):
         email = Email(debug=True)
