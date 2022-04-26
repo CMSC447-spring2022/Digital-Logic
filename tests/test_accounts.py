d7451968fe26f9863a2de48f2d967db651a293ec
Kes Johnson <kes.j.johnson@gmail.com>
2022-04-26 17:01:48 -0400

    started tests

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -10,6 +10,12 @@ class SignUpViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
+class DeleteUserViewTest(TestCase):
+    def test_delete_user_page(self):
+        response = self.client.get('/accounts/delete_account/')
+        self.assertEqual(response.status_code, 302)
+
+
 class SignUpTest(TestCase):
     def setUp(self):
         self.username = 'testuser'
@@ -31,3 +37,13 @@ class SignUpTest(TestCase):
 
         users = get_user_model().objects.all()
         self.assertEqual(users.count(), 1)
+
+
+class DeleteUserTest(TestCase):
+    def test_delete_get(self):
+        user = get_user_model().objects.create_user(username='test', password='123abc456', email='test@example.com')
+        user.save()
+        response = self.client.post('/accounts/delete_account/')
+        self.assertIs()
+
+
