579970b406d47623a167a90e0e3d0e38100cac66
Raj-Karsalia <90571913+Raj-Karsalia@users.noreply.github.com>
2022-04-27 16:13:38 -0400

    created a test.py that uses Selenium

---
+++ LoginPage/test.py
@@ -0,0 +1,29 @@
+from contextlib import contextmanager
+import time
+
+from django.test import override_setting, TestCase
+from django.urls import reverse
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from models.py
+#put our url in here once we have a url
+#page.get("https://ourURLhere.com")
+
+#tests login
+def test_login(self):
+    with self.browser("index") as page:
+        elem = page.find_element_by_id('Username')  # Find the search box
+        elem.send_keys('admin') #inputs admin as user
+        elem = page.find_element_by_id('Password')
+        elem.send_keys('password') #inputs password as input
+        elem.submit() #submits login info
+        
+        self.assertIsNotNone(elem)
+
+#tests create account
+def test_create_account(self):
+    with self.browser("index") as page:
+        elem = page.find_element_by_id("Create Account").click()
