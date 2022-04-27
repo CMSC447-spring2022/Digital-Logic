b94b9ca7e02d3e98d478fbbe9eb43587967388f9
Matt <mmchugh1@umbc.edu>
2022-04-27 16:33:48 -0400

    filestructure updates - removed LoginPage directory - unneded

--- LoginPage_del/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.login_user, name='login_user'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-]
