a1339969cce38bfc63bfb267e7516e0edd838f36
MMcHugh1 <90572892+MMcHugh1@users.noreply.github.com>
2022-04-28 18:10:23 -0400

    Login page matt (#107)
    * login page design files and display
    
    * commit test
    
    * update
    
    * update to LoginPage file structure
    
    * readme update for login_page_design_matt branch
    
    * url and html updates
    
    Signed-off-by: Matt <mmchugh1@umbc.edu>
    
    * misc updates to html files
    
    * login page style and redirects working. style.css file not working.
    
    * fixed styleing, background image still not showing for some reason
    
    * filestructure updates - removed LoginPage directory - unneded
    
    * Assisted in figuring out login page design (#96)
    
    * made some edits like centering the text
    
    * should update the background image
    
    * Implemented CSS boot strap
    
    * created a test.py that uses Selenium
    
    * updated the test but its still not complete
    
    * updated the current html with a CSS bootstep 
    
    Updated the currently HTML file to us the bootstrap template but the background still doesn't work
    
    * updated test page
    
    * Delete style.css
    
    dont need
    
    * Delete login.html
    
    dont need
    
    Co-authored-by: MMcHugh1 <90572892+MMcHugh1@users.noreply.github.com>
    
    * reset some defaults i changed while working
    
    * Delete .gitignore
    
    ide files dont need
    
    * Delete Project_Default.xml
    
    ide files dont need
    
    * Delete profiles_settings.xml
    
    ide files dont need
    
    * Delete misc.xml
    
    ide files dont need
    
    * Delete modules.xml
    
    ide files dont need
    
    * Delete project_development.iml
    
    ide files dont need
    
    * Delete vcs.xml
    
    ide files dont need
    
    * Delete db.sqlite3
    
    * Delete __pycache__ directory
    
    ide files dont need
    
    * Delete DigitalLogic/__pycache__ directory
    
    ide files dont need
    
    * Delete Launcher/__pycache__ directory
    
    ide files dont need
    
    * Delete accounts/__pycache__ directory
    
    ide files dont need
    
    * Delete Launcher/migrations/__pycache__ directory
    
    ide files, not needed
    
    * Delete accounts/migrations/__pycache__ directory
    
    ide files, not needed
    
    * Delete background-2.gif
    
    ide files, not needed
    
    * Delete background.gif
    
    ide files, not needed
    
    * Delete background-2.gif
    
    ide files, not needed
    
    * Delete background.gif
    
    ide files, not needed
    
    * Delete polls/__pycache__ directory
    
    ide files, not needed
    
    * hope this works
    
    Signed-off-by: Matt <mmchugh1@umbc.edu>
    
    Co-authored-by: Raj-Karsalia <90571913+Raj-Karsalia@users.noreply.github.com>
    Co-authored-by: rhiles1 <69433116+rhiles1@users.noreply.github.com>

--- DigitalLogic/urls.py
+++ DigitalLogic/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +10,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('launcher', include("Launcher.urls"), name='index'),
 ]
+
+# urlpatterns += staticfiles_urlpatterns()
